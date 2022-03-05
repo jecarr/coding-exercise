dc2c1f4f03a5bf31a15d044ba06bb7eec6453fe8
jecarr <jenine_c@outlook.com>
2022-03-05 17:56:31 +1300

    Renamed join-strings and updated 404.html

--- exercise/exercise/urls.py
+++ exercise/exercise/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("hello/", views.hello_world, name="hello"),
     # REST endpoints to return combination of arguments
     path("add-numbers/<num1>/<num2>/", views.add_numbers, name="add_numbers"),
-    path("join-strings/<s1>/<s2>/", views.join_words, name="join_words"),
+    path("join-words/<s1>/<s2>/", views.join_words, name="join_words"),
 ]
