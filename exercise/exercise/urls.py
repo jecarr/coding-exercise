f3166f8f0dd7b2798b46d2cae54b28ba1328d968
jecarr <jenine_c@outlook.com>
2022-03-05 17:36:24 +1300

    Added remaining endpoints

--- exercise/exercise/urls.py
+++ exercise/exercise/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from exercise import views
 
 urlpatterns = [
-    path('hello', views.hello_world, name='hello'),
+    # REST endpoint to return 'Hello World'
+    path('hello/', views.hello_world, name='hello'),
+    # REST endpoints to return combination of arguments
+    path('add-numbers/<num1>/<num2>/', views.add_numbers, name='add_numbers'),
+    path('join-strings/<s1>/<s2>/', views.join_words, name='join_words'),
 ]
--- exercise/exercise/views.py
+++ exercise/exercise/views.py
@@ -1,7 +1,36 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
+
+# Could only allow GETs (to imply incoming POSTs won't be processed) for these views?
 
 
 def hello_world(request):
     """The REST endpoint to return 'Hello World'."""
-    # Could only allow GETs (to imply incoming POSTs won't be processed)?
     return HttpResponse('Hello World')
+
+
+# Could supplement add_numbers() and join_words() with additional endpoints
+# that take args in a POST request (rather than in the URL)
+
+def add_numbers(request, num1, num2):
+    """The REST endpoint to return the sum of two numbers.
+    Limitations: Does not accept fractions (e.g. a str-arg of '(1/3)' or '3/4');
+    Also does not allow multiple minus signs (e.g. permits '-1' but not '--1').
+    """
+    # Put the arguments in a list (will use in sum() or for error-response)
+    str_params = [num1, num2]
+    # Convert the arguments into floats (to allow decimals); collate into new list; and sum the list
+    try:
+        result = sum([float(n) for n in str_params])
+    except ValueError:
+        # If any argument is not a number, return a 400 response
+        return HttpResponseBadRequest('Non numerical value(s) supplied in url: ' + ', '.join(str_params))
+    # If all arguments were numbers, return the sum
+    # For tidiness, remove trailing '.0's
+    return_val = int(result) if str(result).endswith('.0') else result
+    return HttpResponse(return_val)
+
+
+def join_words(request, s1, s2):
+    """The REST endpoint to return the concatenation of two strings."""
+    # Return the two string-args with a dash joining them
+    return HttpResponse('{}-{}'.format(s1, s2))
