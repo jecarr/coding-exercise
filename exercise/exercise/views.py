48fe58da19880d94dc5520d4c886c1f2cfd4129e
jecarr <jenine_c@outlook.com>
2022-03-05 16:55:31 +1300

    Removed imports and !s

--- exercise/exercise/views.py
+++ exercise/exercise/views.py
@@ -2,6 +2,6 @@ from django.http import HttpResponse
 
 
 def hello_world(request):
-    """The REST endpoint to return 'Hello World!'."""
+    """The REST endpoint to return 'Hello World'."""
     # Could only allow GETs (to imply incoming POSTs won't be processed)?
-    return HttpResponse('Hello World!')
+    return HttpResponse('Hello World')
